920fe6fae12c7f748aef329528212b1ce244739a
Ian Hart <ianhart13@gmail.com>
2022-10-13 16:14:43 -0400

    added API keys and made basic form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const MY_KEY = process.env.REACT_APP_KEY;
+	const MY_ID = process.env.REACT_APP_ID;
+
+	const exampleReq = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${MY_ID}}&app_key=${MY_KEY}&diet=balanced`
+
   return (
-    <div className="App">
-       <h1>Hello React</h1>
+    <div className='App'>
+      <form className="search-form">
+				<input className='search-bar' type="text" />
+				<button className='search-button' type="submit">Search</button>
+			</form>
     </div>
   );
 }
